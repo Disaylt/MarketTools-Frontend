51e066d76971f9fe38e8efc48b25f974263aa0bc
Disaylt <disofban@gmail.com>
2023-12-18 13:18:31 +0300

    feat: create files for develop and deploy

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
-FROM node:14.15.4
+FROM node:20.10.0-alpine AS build
 WORKDIR /usr/src/app
 COPY package.json package-lock.json ./
 RUN npm install
 COPY . .
-RUN npm run build -- --prod
-EXPOSE 4200
-CMD [ "npm", "run", "start" ]
+RUN npm run build
+
+FROM nginx:latest
+COPY nginx.conf /etc/nginx/nginx.conf
+COPY --from=build /usr/src/app/dist/market-tools-frontend/browser /usr/share/nginx/html
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, isDevMode } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./core/components/header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, HeaderComponent]
 })
 export class AppComponent {
   title = 'MarketTools-Frontend';
+  isDev = isDevMode();
+  
+  nums : number[] = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 }
