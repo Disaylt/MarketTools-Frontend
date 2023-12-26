b599936fa69296daad5babaefa198cafa00f3630
Disaylt <disofban@gmail.com>
2023-12-26 22:10:58 +0300

    init: create icon button:

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -4,13 +4,14 @@ import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angu
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenav } from '@angular/material/sidenav';
 import { WindowsSizes } from '../../constants/window-sizes';
+import { IconButtonComponent } from "../../../shared/components/buttons/icon-button/icon-button.component";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [MatIconModule, CommonModule],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss',
+    imports: [MatIconModule, CommonModule, IconButtonComponent]
 })
 export class HeaderComponent {
   @Output() toggleEvent = new EventEmitter<void>();
---
+++ src/app/shared/components/buttons/icon-button/icon-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-icon-button',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './icon-button.component.html',
+  styleUrl: './icon-button.component.scss'
+})
+export class IconButtonComponent {
+
+}
