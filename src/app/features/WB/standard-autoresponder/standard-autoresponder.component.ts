b6b537d82a25b9ef4e655a1352e45c7d39466eb0
Disaylt <disofban@gmail.com>
2023-12-26 21:40:49 +0300

    init: create tab-bar component

--- src/app/features/WB/standard-autoresponder/standard-autoresponder.component.ts
+++ src/app/features/WB/standard-autoresponder/standard-autoresponder.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { TabBarComponent } from "../../../shared/components/tab-bar/tab-bar.component";
 
 @Component({
-  selector: 'app-standard-autoresponder',
-  standalone: true,
-  imports: [],
-  templateUrl: './standard-autoresponder.component.html',
-  styleUrl: './standard-autoresponder.component.scss'
+    selector: 'app-standard-autoresponder',
+    standalone: true,
+    templateUrl: './standard-autoresponder.component.html',
+    styleUrl: './standard-autoresponder.component.scss',
+    imports: [TabBarComponent]
 })
 export class StandardAutoresponderComponent {
 
---
+++ src/app/shared/components/tab-bar/tab-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-tab-bar',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './tab-bar.component.html',
+  styleUrl: './tab-bar.component.scss'
+})
+export class TabBarComponent {
+
+}
