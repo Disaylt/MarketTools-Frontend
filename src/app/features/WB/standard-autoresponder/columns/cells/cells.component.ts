93886f61fc5487606735526d6c947a5498dc7647
Disaylt <disofban@gmail.com>
2023-12-29 03:19:25 +0300

    init: create progress bar

--- src/app/features/WB/standard-autoresponder/columns/cells/cells.component.ts
+++ src/app/features/WB/standard-autoresponder/columns/cells/cells.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import {CdkMenu, CdkMenuItem, CdkMenuTrigger} from '@angular/cdk/menu';
 import { ToastrService } from 'ngx-toastr';
+import { ProgressBarComponent } from "../../../../../shared/components/progress-bar/progress-bar.component";
 
 @Component({
     selector: 'app-cells',
     standalone: true,
     templateUrl: './cells.component.html',
     styleUrl: './cells.component.scss',
-    imports: [CdkMenuTrigger, CdkMenu, CdkMenuItem]
+    imports: [CdkMenuTrigger, CdkMenu, CdkMenuItem, ProgressBarComponent]
 })
 export class CellsComponent {
     
