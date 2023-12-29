73b33967655ccb303765f189751a6916ca79f715
Disaylt <disofban@gmail.com>
2023-12-29 05:23:43 +0300

    init: create cell models

--- src/app/features/WB/standard-autoresponder/columns/cells/cells.component.ts
+++ src/app/features/WB/standard-autoresponder/columns/cells/cells.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import {CdkMenu, CdkMenuItem, CdkMenuTrigger} from '@angular/cdk/menu';
 import { ToastrService } from 'ngx-toastr';
 import { ProgressBarComponent } from "../../../../../shared/components/progress-bar/progress-bar.component";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-cells',
     standalone: true,
     templateUrl: './cells.component.html',
     styleUrl: './cells.component.scss',
-    imports: [CdkMenuTrigger, CdkMenu, CdkMenuItem, ProgressBarComponent]
+    imports: [CdkMenuTrigger, CdkMenu, CdkMenuItem, ProgressBarComponent, CommonModule, FormsModule]
 })
 export class CellsComponent {
+    
+    private _columnId : number | null = null;
+
+    @Input()
+    set columnId(value : number | null){
+        this._columnId = value;
+        console.log(value);
+    }
+
+    isLoad : boolean = false;
+
     
 }
