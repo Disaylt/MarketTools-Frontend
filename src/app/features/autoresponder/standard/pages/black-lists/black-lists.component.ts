02e20967e30a6567c66deac5fb5b55225b219111
Disaylt <disofban@gmail.com>
2024-01-08 06:45:00 +0300

    init: create black lists compoent

---
+++ src/app/features/autoresponder/standard/pages/black-lists/black-lists.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-black-lists',
+  standalone: true,
+  imports: [],
+  templateUrl: './black-lists.component.html',
+  styleUrl: './black-lists.component.scss'
+})
+export class BlackListsComponent {
+
+}
