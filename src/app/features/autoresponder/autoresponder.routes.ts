f752a18f61ea198b75d9fec2b08b80da4cd3a44a
Disaylt <disofban@gmail.com>
2024-01-08 07:33:55 +0300

    init: create black lists service

--- src/app/features/autoresponder/autoresponder.routes.ts
+++ src/app/features/autoresponder/autoresponder.routes.ts
@@ -3,6 +3,7 @@ import { StandardComponent } from "./standard/standard.component";
 import { ColumnsComponent } from "./standard/pages/columns/columns.component";
 import { RecommendationTableComponent } from "./standard/pages/recommendation-table/recommendation-table.component";
 import { CellsComponent } from "./standard/pages/columns/pages/cells/cells.component";
+import { BlackListsComponent } from "./standard/pages/black-lists/black-lists.component";
 
 export const autoresponderRoutes : Routes = [
     {
@@ -19,6 +20,11 @@ export const autoresponderRoutes : Routes = [
                 title : "Таблица рекомендаций",
                 path: "recommendation-table",
                 component:RecommendationTableComponent
+            },
+            {
+                title : "Черный список",
+                path : "black-lists",
+                component : BlackListsComponent
             }
         ]
     }
--- src/app/features/autoresponder/standard/pages/black-lists/black-lists.component.ts
+++ src/app/features/autoresponder/standard/pages/black-lists/black-lists.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-black-lists',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './black-lists.component.html',
   styleUrl: './black-lists.component.scss'
 })
 export class BlackListsComponent {
-
+  newName : string = "";
 }
