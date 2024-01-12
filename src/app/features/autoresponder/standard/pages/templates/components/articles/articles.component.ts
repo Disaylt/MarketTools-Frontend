f7a9b9b0422ce2752863b0520f979317e47183aa
Disaylt <disofban@gmail.com>
2024-01-12 19:02:28 +0300

    feat: implement articles compoent

--- src/app/features/autoresponder/standard/pages/templates/components/articles/articles.component.ts
+++ src/app/features/autoresponder/standard/pages/templates/components/articles/articles.component.ts
@@ -1,15 +1,137 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ArticlesService } from './services/articles.service';
+import { SpinerComponent } from "../../../../../../../shared/components/spiner/spiner.component";
+import { ViewResult } from '../../../../../../../core/models/view-result.model';
+import { ArticleCreateModel, ArticleVm } from './models/article.models';
+import { finalize, map } from 'rxjs';
+import { ViewMappingService } from '../../../../../../../core/services/view-mapping.service';
+import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
 
 @Component({
-  selector: 'app-articles',
-  standalone: true,
-  imports: [CommonModule, FormsModule],
-  templateUrl: './articles.component.html',
-  styleUrl: './articles.component.scss'
+    selector: 'app-articles',
+    standalone: true,
+    templateUrl: './articles.component.html',
+    styleUrl: './articles.component.scss',
+    imports: [CdkMenuTrigger, CdkMenu, CdkMenuItem, CommonModule, FormsModule, SpinerComponent]
 })
-export class ArticlesComponent {
-  value : string = "";
+export class ArticlesComponent{
+
+  private _templateId! : number;
+
+  newArticle = {
+    value : "",
+    isLoad : false
+  }
+  editTextArea : string = "";
   isEditMode : boolean = false;
+  isLoad : boolean = true;
+  articles : ViewResult<ArticleVm>[] = [];
+
+  @Input({required : true})
+    set templateId(value : number){
+      this._templateId = value;
+      this.isEditMode = false;
+      this.getRange();
+    }
+
+  constructor(private articlesServise : ArticlesService, private mapper : ViewMappingService){}
+
+  delete(article : ViewResult<ArticleVm>){
+    article.actions.isLoad = true;
+
+    this.articlesServise.delete(article.data.id)
+      .pipe(
+        finalize(() => {
+          article.actions.isLoad = false;
+        })
+      )
+      .subscribe({
+        complete : () => {
+          this.articles = this.articles
+            .filter(x=> x != article);
+        }
+      })
+  }
+
+  saveNewArticles(){
+    this.isLoad = true;
+    const newArticles : string[] = this.editTextArea
+      .split("\n");
+    
+    this.articlesServise.updateRange(newArticles, this._templateId)
+      .pipe(
+        map(data => data.map(value => this.mapper.map(value))),
+        finalize(() => {
+          this.isLoad = false;
+        })
+      )
+      .subscribe({
+        next: data => {
+          this.articles = data;
+        },
+        complete : () => {
+          this.isEditMode = false;
+        }
+      })
+  }
+
+  changeEditMode(){
+    this.isEditMode = !this.isEditMode;
+    this.editTextArea = "";
+
+    if(this.isEditMode){
+      this.articles.forEach(x=> {
+        this.editTextArea += `${x.data.value}\r\n`
+      })
+    }
+  }
+
+  add() {
+    this.newArticle.isLoad = true;
+
+    const body : ArticleCreateModel = {
+      value : this.newArticle.value,
+      templateId : this._templateId
+    }
+
+    this.articlesServise.create(body)
+      .pipe(
+        map(data => this.mapper.map(data)),
+        finalize(() => {
+          this.newArticle.isLoad = false;
+        })
+      )
+      .subscribe(
+        {
+          next: data => {
+            if(this._templateId == body.templateId){
+              this.articles.push(data);
+            }
+          },
+          complete : ()=> {
+            this.newArticle.value = "";
+          }
+        }
+      )
+  }
+
+  private getRange(){
+    this.isLoad = true;
+    this.articles = [];
+
+    this.articlesServise.getRange(this._templateId)
+      .pipe(
+        map(data => data.map(value => this.mapper.map(value))),
+        finalize(() => {
+          this.isLoad = false;
+        })
+      )
+      .subscribe({
+        next : data => {
+          this.articles = data;
+        }
+      })
+  }
 }
--- src/app/features/autoresponder/standard/pages/templates/components/articles/services/articles.service.ts
+++ src/app/features/autoresponder/standard/pages/templates/components/articles/services/articles.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ArticleCreateModel } from '../models/article.models';
+import { ArticleCreateModel, ArticleVm } from '../models/article.models';
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +12,19 @@ export class ArticlesService {
   updateRange(articles : string[], templateId : number){
     const path = `api/v1/autoresponder/standard/template/articles/update?templateId=${templateId}`;
 
-    return this.httpClient.put(path, articles);
+    return this.httpClient.put<ArticleVm[]>(path, articles);
+  }
+
+  getRange(templateId : number){
+    const path = `api/v1/autoresponder/standard/template/articles?templateId=${templateId}`;
+
+    return this.httpClient.get<ArticleVm[]>(path);
   }
 
   create(body : ArticleCreateModel) {
     const path = `api/v1/autoresponder/standard/template/article`;
 
-    return this.httpClient.post(path, body);
+    return this.httpClient.post<ArticleVm>(path, body);
   }
 
   delete(id : number){
