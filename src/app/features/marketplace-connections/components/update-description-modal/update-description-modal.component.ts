260b6b9c786f9f29c4e2d9182594eff5f36fde43
Disaylt <disofban@gmail.com>
2024-01-19 23:58:07 +0300

    init: create update description modal

---
+++ src/app/features/marketplace-connections/components/update-description-modal/update-description-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-description-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-description-modal.component.html',
+  styleUrl: './update-description-modal.component.scss'
+})
+export class UpdateDescriptionModalComponent {
+
+}
