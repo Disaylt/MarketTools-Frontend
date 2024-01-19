67f3978ebc94144292ef206ef652c89fe890a824
Disaylt <disofban@gmail.com>
2024-01-19 13:08:51 +0300

    init: create and implement get/delete methods for marketplace connections

---
+++ src/app/core/enums/marketplace-connection.enum.ts
@@ -0,0 +1,3 @@
+export enum MarketplaceConnection{
+    wbSellerOpenApi
+}
---
+++ src/app/features/marketplace-connections/services/marketplace-connections.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MarketplaceConnection } from '../../../core/enums/marketplace-connection.enum';
+import { MarketplaceConnectionModel } from '../models/marketplace-connection.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MarketplaceConnectionsService {
+
+  constructor(private httpClient : HttpClient) { }
+
+  delete(id : number){
+    const path = `api/v1/marketplace-connection?id=${id}`;
+
+    return this.httpClient.delete(path);
+  }
+
+  getRange(type : MarketplaceConnection, skip : number, take : number){
+    const path = `api/v1/marketplace-connections?connectionType=${type}&take=${take}&skip=${skip}`;
+
+    return this.httpClient.get<MarketplaceConnectionModel[]>(path);
+  }
+}
