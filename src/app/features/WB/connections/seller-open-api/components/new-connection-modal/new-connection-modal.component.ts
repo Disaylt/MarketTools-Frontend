ddffe5018a4b47eb228bdba83cda65f0ea907ce3
Disaylt <disofban@gmail.com>
2024-01-19 13:15:02 +0300

    init: create new connection modal

---
+++ src/app/features/WB/connections/seller-open-api/components/new-connection-modal/new-connection-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-connection-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-connection-modal.component.html',
+  styleUrl: './new-connection-modal.component.scss'
+})
+export class NewConnectionModalComponent {
+
+}
