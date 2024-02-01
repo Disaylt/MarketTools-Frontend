c63a53e7ffe3bd9bbd9796f4e55276606c985083
Disaylt <disofban@gmail.com>
2024-02-01 07:04:29 +0300

    init: create response testing component

---
+++ src/app/features/autoresponder/standard/pages/response-testing/response-testing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-response-testing',
+  standalone: true,
+  imports: [],
+  templateUrl: './response-testing.component.html',
+  styleUrl: './response-testing.component.scss'
+})
+export class ResponseTestingComponent {
+
+}
