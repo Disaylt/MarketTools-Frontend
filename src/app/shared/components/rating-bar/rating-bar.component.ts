076d0d05468fc965d896205e1f84d9e2833f76d3
Disaylt <disofban@gmail.com>
2024-02-01 23:25:46 +0300

    feat: implement ratings bar

--- src/app/shared/components/rating-bar/rating-bar.component.ts
+++ src/app/shared/components/rating-bar/rating-bar.component.ts
@@ -1,13 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-rating-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './rating-bar.component.html',
   styleUrl: './rating-bar.component.scss'
 })
-export class RatingBarComponent {
+export class RatingBarComponent implements OnInit{
+
+  @Output() changeRating : EventEmitter<number> = new EventEmitter();
+  @Input() selectRate : number = 0;
   @Input() maxRate : number = 5;
+
+  hoverRate : number = 0;
+  rates : number[] = []
+
+  ngOnInit(): void {
+    this.setRates(); 
+  }
+
+  change(rate : number){
+    this.changeRating.emit(rate);
+  }
+
+  isActive(rate : number){
+    return rate <= this.selectRate;
+  }
+
+  isHover(rate : number){
+    return rate <= this.hoverRate;
+  }
+
+  private setRates(){
+    for(let rate =1 ; rate <= this.maxRate; rate ++){
+      this.rates.push(rate);
+    }
+  }
 }
