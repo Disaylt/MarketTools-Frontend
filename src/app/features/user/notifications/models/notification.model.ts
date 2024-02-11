2ce0b09f2a9f6c22b809294f6b57cfd2e888836e
Disaylt <disofban@gmail.com>
2024-02-11 20:49:43 +0300

    init: create notifications service

---
+++ src/app/features/user/notifications/models/notification.model.ts
@@ -0,0 +1,10 @@
+export interface NotificationModel{
+    id : number;
+    createDate : string;
+    text : string;
+    isRead : boolean;
+}
+
+export interface TotalNotifications{
+    value : number;
+}
