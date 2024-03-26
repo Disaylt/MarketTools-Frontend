99831fc911841a867f2b2e856350fc0bf9079a17
Disaylt <disofban@gmail.com>
2024-03-26 14:53:07 +0300

    feat: add main status for autoresponder settings

--- src/app/features/autoresponder/standard/pages/templates/components/settings/models/settings.model.ts
+++ src/app/features/autoresponder/standard/pages/templates/components/settings/models/settings.model.ts
@@ -1,6 +1,7 @@
 export interface SettingsModel{
     isSkipWithTextFeedbacks : boolean;
     isSkipEmptyFeedbacks : boolean;
+    isMain : boolean;
 }
 
 export interface SettingsUpdateModel extends SettingsModel{
