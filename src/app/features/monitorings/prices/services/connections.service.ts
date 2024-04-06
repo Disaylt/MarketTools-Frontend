d1d31649110c7ff21efc35e8eaeaf8785f00fb7e
Disaylt <disofban@gmail.com>
2024-04-06 04:57:53 +0300

    feat: add http methods

--- src/app/features/monitorings/prices/services/connections.service.ts
+++ src/app/features/monitorings/prices/services/connections.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SettingsModel, SettingsUpdateModel } from '../models/settings.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,4 +14,17 @@ export class ConnectionsService {
 
     return this.httpClient.put(path, null);
   }
+
+  getSettings(id :number){
+    const path = `api/v1/monitoring/price/connection/settings?connectionId=${id}`; 
+
+    return this.httpClient.get<SettingsModel>(path);
+  }
+
+  updateSettings(settings : SettingsUpdateModel){
+    const path = `api/v1/monitoring/price/connection/settings`; 
+
+    return this.httpClient.put<SettingsModel>(path, settings);
+  }
+
 }
