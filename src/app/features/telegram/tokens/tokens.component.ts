980327c762a18c956c280589fda51a7f26fe410d
Disaylt <disofban@gmail.com>
2024-04-06 11:01:30 +0300

    init: create model

--- src/app/features/telegram/tokens/tokens.component.ts
+++ src/app/features/telegram/tokens/tokens.component.ts
@@ -1,3 +1,4 @@
+import { ClipboardModule } from '@angular/cdk/clipboard';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -5,11 +6,11 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-tokens',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, ClipboardModule],
   templateUrl: './tokens.component.html',
   styleUrl: './tokens.component.scss'
 })
 export class TokensComponent {
   isLoad : boolean = false;
-  token : string | null = "3242-6asdd3-423rsr-4234";
+  token : string | null = null;
 }
