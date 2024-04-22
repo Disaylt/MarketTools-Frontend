6d92207bf3270f5b1d99fbb4220e5f632d52bfed
Disaylt <disofban@gmail.com>
2024-04-22 15:47:07 +0300

    init: service

--- src/app/features/WB/workspace/price/Services/price.service.ts
+++ src/app/features/WB/workspace/price/Services/price.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PriceDetailsResult } from '../models/result.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PriceService {
 
-  constructor() { }
+  constructor(private httpClient : HttpClient) { }
+
+  getRange(connectionId : number){
+    const path = `api/v1/workspace/price?connectionId=${connectionId}`;
+
+    return this.httpClient.get<PriceDetailsResult[]>(path);
+  }
 }
