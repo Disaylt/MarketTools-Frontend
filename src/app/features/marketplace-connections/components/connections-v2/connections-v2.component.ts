9a988f8367ca9893baa9a78e1013ca0049a94500
Disaylt <disofban@gmail.com>
2024-05-08 17:55:30 +0300

    init: create connections v2

---
+++ src/app/features/marketplace-connections/components/connections-v2/connections-v2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connections-v2',
+  standalone: true,
+  imports: [],
+  templateUrl: './connections-v2.component.html',
+  styleUrl: './connections-v2.component.scss'
+})
+export class ConnectionsV2Component {
+
+}
