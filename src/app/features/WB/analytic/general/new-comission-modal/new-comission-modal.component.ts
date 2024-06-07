a02e268dbac5224a483343fc7ad614a90d55b672
Disaylt <disofban@gmail.com>
2024-06-07 08:14:27 +0300

    init: create component

---
+++ src/app/features/WB/analytic/general/new-comission-modal/new-comission-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-comission-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-comission-modal.component.html',
+  styleUrl: './new-comission-modal.component.scss'
+})
+export class NewComissionModalComponent {
+
+}
