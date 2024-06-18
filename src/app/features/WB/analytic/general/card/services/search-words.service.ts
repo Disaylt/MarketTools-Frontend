de7619ca271ccf521da6349a7a95ae3d0f229eae
Disaylt <disofban@gmail.com>
2024-06-18 11:07:24 +0300

    init: create service

---
+++ src/app/features/WB/analytic/general/card/services/search-words.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SearchWordVm } from '../../cards/models/search-word.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchWordsService {
+
+  constructor(private httpClient : HttpClient) { }
+
+  addWord(cardId : number, name : string){
+    const path = `api/v1/analytic/general/card/searchword`;
+    const body = {
+      cardId : cardId,
+      value : name
+    };
+
+    return this.httpClient.post<SearchWordVm>(path, body);
+  }
+
+  delete(id : number){
+    const path = `api/v1/analytic/general/card/searchword`;
+
+    return this.httpClient.delete(path);
+  }
+}
